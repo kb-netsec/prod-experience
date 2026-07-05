6c371ae41ad38e3c8a57baf1c2a81a1b6bbb7364
kb-netsec <kb-netsec@proton.me>
2026-07-05 11:07:52 -0400

    Add vyos-vlan-post-check.py for VLAN validation
    Script checks VLAN configuration on VyOS and alerts for discrepancies.

---
+++ scripts/vyos-vlan-post-check.py
@@ -0,0 +1,7 @@
+1. request vlan id from user
+2. log into vyos primary and backup
+3. run show config commands to get outputs
+4. dump to file
+5. diff against mop command list
+6. show diffs and also alert if there is any missing commands
+
