3ac01b2c0bc8c078c8ed1f438c8a9b33c70a416e
kb-netsec <kb-netsec@proton.me>
2026-07-05 21:26:13 -0400

    Update create-vyos-vlan.py

--- scripts/create-vyos-vlan.py
+++ scripts/create-vyos-vlan.py
@@ -1,1 +1,9 @@
+# This scrpt creates a VyOS config with default policy template
+
+# Query user for VLAN ID 
+# Create vlan id variable
+query user for network and mask
+create network/mask variable
+
+set firewall group network-group NET-4-VLAN838 network '172.20.38.0/24'
 
