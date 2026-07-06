10ea305c4f6964a5fbbecfd81f062391194dd142
kb-netsec <kb-netsec@proton.me>
2026-07-05 22:44:07 -0400

    Add placeholder for missing step in VLAN check script
    Added a placeholder for a missing step in the script.

--- scripts/vyos-vlan-post-check.py
+++ scripts/vyos-vlan-post-check.py
@@ -1,8 +1,15 @@
 1. request vlan id from user
 2. log into vyos primary
 3. query hostname and vrrp to ensure expected (primary, 1a) 
-4.
+4. ? forgot
 5. run show config commands to get outputs
+
+
+show configuration commmands | grep 'set firewall group network-group NET-4-VLANXXX'
+show configuration commands | grep VLANXXX-4-IN
+show configuration commands | grep VLANXXX-4-OUT
+
+
 6. dump to file
 7. request mop commnad list file
 8. diff against mop command list
