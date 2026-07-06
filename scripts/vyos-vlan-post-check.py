286e5b3f997e501be322f99a7b8f5ba0a807392c
kb-netsec <kb-netsec@proton.me>
2026-07-05 22:10:38 -0400

    Update vyos-vlan-post-check.py

--- scripts/vyos-vlan-post-check.py
+++ scripts/vyos-vlan-post-check.py
@@ -1,7 +1,10 @@
 1. request vlan id from user
-2. log into vyos primary and backup
-3. run show config commands to get outputs
-4. dump to file
-5. diff against mop command list
-6. show diffs and also alert if there is any missing commands
+2. log into vyos primary
+3. query hostname and vrrp to ensure expected (primary, 1a) 
+4.
+5. run show config commands to get outputs
+6. dump to file
+7. request mop commnad list file
+8. diff against mop command list
+9. show diffs and also alert if there is any missing commands
 
